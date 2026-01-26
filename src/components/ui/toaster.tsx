9fec9343cff6e232fe4fa3421418b4d9da600a95
Muhammad Fakhrul Amin <80448871+mfakhrulam@users.noreply.github.com>
2026-01-26 20:14:29 +0700

    fix: toast

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,20 +1,43 @@
-import { Toaster as ChakraToaster, Toast, createToaster } from '@chakra-ui/react';
+"use client"
+
+import {
+  Toaster as ChakraToaster,
+  Portal,
+  Spinner,
+  Stack,
+  Toast,
+  createToaster,
+} from "@chakra-ui/react"
 
 export const toaster = createToaster({
-  placement: 'top-end',
+  placement: "top-end",
   pauseOnPageIdle: true,
-});
+})
 
 export const Toaster = () => {
   return (
-    <ChakraToaster toaster={toaster}>
-      {(toast) => (
-        <Toast.Root>
-          {toast.title && <Toast.Title>{toast.title}</Toast.Title>}
-          {toast.description && <Toast.Description>{toast.description}</Toast.Description>}
-          <Toast.CloseTrigger />
-        </Toast.Root>
-      )}
-    </ChakraToaster>
-  );
-};
+    <Portal>
+      <ChakraToaster toaster={toaster} insetInline={{ mdDown: "4" }}>
+        {(toast) => (
+          <Toast.Root width={{ md: "sm" }}>
+            {toast.type === "loading" ? (
+              <Spinner size="sm" color="blue.solid" />
+            ) : (
+              <Toast.Indicator />
+            )}
+            <Stack gap="1" flex="1" maxWidth="100%">
+              {toast.title && <Toast.Title>{toast.title}</Toast.Title>}
+              {toast.description && (
+                <Toast.Description>{toast.description}</Toast.Description>
+              )}
+            </Stack>
+            {toast.action && (
+              <Toast.ActionTrigger>{toast.action.label}</Toast.ActionTrigger>
+            )}
+            {toast.closable && <Toast.CloseTrigger />}
+          </Toast.Root>
+        )}
+      </ChakraToaster>
+    </Portal>
+  )
+}
